ae9b25ec6160357379fe43635a430a4a66a88899
MartinRep <repickym@pm.me>
2020-10-16 15:06:32 +0200

    CleanUp and agruments process

--- cpu_temp.py
+++ cpu_temp.py
@@ -2,10 +2,12 @@ from subprocess import Popen, PIPE
 import RPi.GPIO as GPIO 
 from time import sleep
 from collections import deque
+import getopt
+import sys
 
 class fan_control():
     
-    def __init__(self, pin):
+    def __init__(self, pin=16):
         self.pin = pin
         GPIO.setwarnings(False) 
         GPIO.setmode(GPIO.BCM)
@@ -23,39 +25,49 @@ class fan_control():
         return self.state
 
 
-
 class cpu_monitor():
 
-    def __init__(self):
-        temp = cpu_monitor.get_temp()
-        self.temperatures = [temp, temp, temp, temp, temp, temp, temp, temp, temp, temp]
+    def __init__(self, trigger_temp=60, delay=5):
+        self.trigger_temp = trigger_temp
+        self.temp = self._temp()
+        self.temperatures = [self.temp for i in range(delay)]
+        
     
-    @staticmethod
-    def get_temp():
+    def _temp(self):
         process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
-        stdout = stdout.decode("utf-8") 
-        temp = stdout[(stdout.find('=')+1):-3]  #Extract value from '=' sign to 3rd character from the end
-        return float(temp)
+        if not stderr:
+            stdout = stdout.decode("utf-8") 
+            temp = stdout[(stdout.find('=')+1):-3]  #Extract value from '=' sign to 3rd character from the end
+            return float(temp)
+        else:
+            return self.trigger_temp
 
     def update(self):
         self.temperatures.pop(0)
-        self.temperatures.append(cpu_monitor.get_temp())
-    
-    def average_temp(self):
-        temp_sum = 0
-        for temp in self.temperatures:
-            temp_sum += temp
-        return temp_sum / len(self.temperatures)
+        self.temperatures.append(self._temp())
+        self.temp = sum(self.temperatures) / len(self.temperatures)
+        return self.temp
+        
 
     
 def main():
-    fan = fan_control(16)
-    monitor = cpu_monitor()
+    trigger_temp = 60
+    delay = 5
+    pin = 16
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, 't:d:p:')
+    for key, value in opts:
+        if(key is 't'):
+            trigger_temp = value
+        if(key is 'd'):
+            delay = value
+        if(key is 'p'):
+            pin = value
+    fan = fan_control(pin)
+    monitor = cpu_monitor(trigger_temp=trigger_temp, delay=delay)
     while True:
-        monitor.update()
-        print(f"Average temperature: {monitor.average_temp()} 'C")
-        if(monitor.average_temp() > 60):
+        if(monitor.update() > trigger_temp):
             fan.on()
         else:
             fan.off()
@@ -64,4 +76,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
